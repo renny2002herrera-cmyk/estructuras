68fa183852d3519770e6c6071938c3d0a98817c4
renny2002herrera-cmyk <renny.2002herrera@gmail.com>
2026-08-21 09:44:18 -0500

    21 august class

---
+++ src/MisAlgoritmos.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+
+public class MisAlgoritmos implements Busquedas,Ordenamientos{
+    @Override
+    public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
+        for (int i= 0; i < arrayPorExplorar.length; i ++){
+            if (arrayPorExplorar[i] == elementoABuscar){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
+        return -1;
+    }
+
+    @Override
+    public int[] bubbleSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] selectionSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] insertionSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] mergeSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+
+    @Override
+    public int[] quickSort(int[] arrayDesordenado) {
+        return new int[0];
+    }
+}
