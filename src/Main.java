e137e5af5761a27cedd75a96ed6803c314449d44
renny2002herrera-cmyk <renny.2002herrera@gmail.com>
2026-08-24 23:47:28 -0500

    advance

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int [] miArray = {70,12,19,45,11,33,26};
+        int[] miArray = {70, 12, 19, 45, 11, 33, 26};
         MisAlgoritmos misAlgoritmos = new MisAlgoritmos();
 
-        System.out.println("Lineal: " + misAlgoritmos.linearSearch(miArray, 45));
-
+        System.out.println("Lineal: " + misAlgoritmos.linearSearch(miArray, 11));
+        System.out.println("binary: " + misAlgoritmos.binarySearch(miArray));
+        System.out.println("bubble sort: " + Arrays.toString(misAlgoritmos.bubbleSort(miArray)));
+        System.out.println("selection sort: " + Arrays.toString(misAlgoritmos.selectionSort(miArray)));
     }
+}
+
--- src/MisAlgoritmos.java
+++ src/MisAlgoritmos.java
@@ -1,30 +1,55 @@
-import java.util.Arrays;
-
-
-public class MisAlgoritmos implements Busquedas,Ordenamientos{
+public class MisAlgoritmos implements Busquedas,Ordenamientos {
     @Override
     public int linearSearch(int[] arrayPorExplorar, int elementoABuscar) {
-        for (int i= 0; i < arrayPorExplorar.length; i ++){
-            if (arrayPorExplorar[i] == elementoABuscar){
+        for (int i = 0; i < arrayPorExplorar.length; i++) {
+            if (arrayPorExplorar[i] == elementoABuscar) {
                 return i;
             }
+
         }
         return -1;
     }
 
     @Override
-    public int binarySearch(int[] arrayPorExplorar, int elementoABuscar) {
-        return -1;
+    public int binarySearch(int[] arrayPorExplorar) {
+        return 0;
     }
 
     @Override
     public int[] bubbleSort(int[] arrayDesordenado) {
-        return new int[0];
+        int[] array = arrayDesordenado.clone();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1 - i; j++) {
+
+                if (array[j] > array[j + 1]) {
+                    int temporal = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temporal;
+                }
+            }
+        }
+        return array;
     }
 
     @Override
     public int[] selectionSort(int[] arrayDesordenado) {
-        return new int[0];
+        int[] array = arrayDesordenado.clone();
+        for (int i = 0; i < array.length - 1; i++) {
+            int posicionMenor = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[posicionMenor]) {
+                    posicionMenor = j;
+                }
+            }
+            {
+                int temporal = array[i];
+                array[i] = array[posicionMenor];
+                array[posicionMenor] = temporal;
+
+            }
+        }
+        return array;
     }
 
     @Override
@@ -42,3 +67,4 @@ public class MisAlgoritmos implements Busquedas,Ordenamientos{
         return new int[0];
     }
 }
+
